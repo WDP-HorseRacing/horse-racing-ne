c2ade0844912926ab1964abd0c3702f51a591524
HarryDo29 <128828116+HarryDo29@users.noreply.github.com>
2026-09-15 10:25:39 +0700

    fix: add eslint directive for unsafe argument in RedisService constructor

--- src/common/redis/redis.service.ts
+++ src/common/redis/redis.service.ts
@@ -4,6 +4,7 @@ import { REDIS_CLIENT } from './redis.token';
 
 @Injectable()
 export class RedisService implements OnModuleDestroy {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   constructor(@Inject(REDIS_CLIENT) private readonly client: Redis) {}
 
   getClient(): Redis {
